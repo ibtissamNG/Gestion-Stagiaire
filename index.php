970880f4264f036c53709fc612302f7b43635ac1
ibtissamNG <102632724+ibtissamNG@users.noreply.github.com>
2022-04-16 12:25:29 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Gestion stagiaire</title>
-	<link rel="stylesheet"  href="css/Style_index1.css">
+	<title>CommuneBerkane</title>
+	<link rel="stylesheet"  href="CSS/Style_index1.css">
 </head>
 <body>
 
    <section class="d1">
-      <h1> <b> Gestion stagiaire </b></h1>
-      <button id="btn1" ><a href="Anonyme/login.php" id="b1">Se Connecter</a></button>
+      <h1>Gestion Des Stagiaires</h1>
+      <button id="btn1"  ><a href="Anonyme/login.php" id="b1">Se Connecter</a></button>
    </section>
+   
 
 
 </body>
