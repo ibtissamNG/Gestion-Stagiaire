92b451518b7b9c02a2c221c7a80b33d970f14dd6
ouissalneqrouz <102753278+ouissalneqrouz@users.noreply.github.com>
2022-04-25 12:17:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Gestion des stagiaires</title>
-	<link rel="stylesheet"  href="CSS/Style_index1.css">
-</head>
-<body>
-
-   <section class="d1">
-      <h1>Gestion Des Stagiaires</h1>
-      <button id="btn1"  ><a href="Anonyme/login.php" id="b1">Se Connecter</a></button>
-   </section>
-   
-
-
-</body>
+  <head>
+  <title>Gestion des stagiaires</title>
+  <link rel="stylesheet"  href="CSS/Style_index.css">
+  </head>
+  <body>
+    <div class="image"></div>
+    <div class="text" style="border-left-width: 2.994;width: 500.724;width: 492px;padding-top: 12px;padding-bottom: 80px;height: 80;bottom: 120px;">
+      <h1 style="margin-top: 3px;">Gestion Des Stagiaires</h1>
+      <div class="txt"style="margin-top: 1px;height: 125px; line-height: 1.6;"><h5 style="margin-top: 10px;">Bienvenue dans votre application qui va vous faciliter la gestion des stagiaires, les responsables qui les supervisent ainsi que les projets dont lesquelles ils sont intégrés</h5><br></div>
+    </div>
+    <div class="connexion">
+      <button id="btn1" ><a href="Anonyme/login.php" id="b1">Se Connecter</a></button>
+   </div>
+  </body>
 </html>
