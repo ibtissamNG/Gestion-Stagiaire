5d47c4a55602db6ef5e05de11dc1c289c93033df
ouissalneqrouz <102753278+ouissalneqrouz@users.noreply.github.com>
2022-04-25 21:26:17 +0000

    Update login.php

--- Anonyme/login.php
+++ Anonyme/login.php
@@ -13,48 +13,62 @@ ob_start();
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link href="../css/style.css" rel="stylesheet">
     <link href="../css/style1.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/fontawesome-all.css"> 
-    <style>
-    html,
+    <link rel="stylesheet" href="../css/fontawesome-all.css">
+<style>
     body {
-        height: 100%;
-    }
-
-    body {
-        display: -ms-flexbox;
-        display: flex;
-        -ms-flex-align: center;
-        align-items: center;
-        padding-top: 40px;
-        padding-bottom: 40px;
-    }
-    .logo-img{
-        height: 90px;
-        width: 95px;
-    }
-    </style>
+    font-family: 'Circular Std Book';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    color: #71748d;
+    background-image: url("../img/landing.jpg");
+    -webkit-font-smoothing: antialiased;
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+  
 </head>
 
-<body>
+
+<body >    
     <!-- ============================================================== -->
     <!-- login page  -->
     <!-- ============================================================== -->
-    <div class="splash-container">
-        <div class="card ">
-            <div class="card-header text-center"><img class="logo-img" src="../img/login.jpg" alt="logo"><span class="splash-description">Veuillez entrer vos informations.</span></div>
-            <div class="card-body">
-                <form  id="connecter"   class="inputs"  method="POST">
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" id="username" type="text" placeholder="Nom d'utilisateur" name="email" autocomplete="off">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" id="password" type="password" placeholder="Mot de passe" name="mot_de_passe">
-                    </div>
-                   
-                    <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Se connecter</button>
-                </form>
-            
-           
+        <section class="vh-100 gradient-custom">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div class="card  text-white" style="border-radius: 1rem;">
+                            <div class="card-body p-5 text-center bg-white">
+                                <div class="mb-md-5 mt-md-4 pb-5">
+                                    <h1 class="fw-bold mb-3 text-uppercase ">Bienvenue</h1>
+                                    <h5 class="fw-thin text-white mb-5">Entrez vos informations!</h5>
+                                    
+                                    <form  id="connecter" class="inputs"  method="POST">
+
+                                        <div class="form-outline form-white mb-4">
+                                            <input id="username" type="text" placeholder="Nom d'utilisateur" name="email" autocomplete="off" class="form-control form-control-lg" style="border-radius:1rem" /> 
+                                        </div>
+
+                                        <div class="form-outline form-white mb-4">
+                                            <input id="password" type="password" placeholder="Mot de passe" name="mot_de_passe" class="form-control form-control-lg" style="border-radius:1rem" />
+                                        </div>
+
+                                        <button class="btn btn-outline-light btn-lg px-5 my-2" type="submit" name="submit">Connexion</button>
+                                   
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+</section>
+             
              <?php
             include ('../Connexion_database.php');
             if (isset($_POST['submit']))
@@ -73,13 +87,13 @@ ob_start();
               }
               if($cpt2==0)
               {
-                echo '<p class="R">votre mot de passe ou email est incorrect !</p>';
+                echo '<p class="R">Votre nom ou mot de passe est incorrect !</p>';
               }
               if($cpt2!=0)
               {
                 session_start();
                 $_SESSION['monlogin'] =$Email;
-                header('location: ../User/AfficherService.php');
+                header('location: ../User/AfficherStagiaire.php');
               }
             }
             mysqli_close($connexion);
@@ -91,9 +105,11 @@ ob_start();
     <!-- ============================================================== -->
     <!-- end login page  -->
     <!-- ============================================================== -->
+ 
 
      <?php
      ob_end_flush();
    ?>
 </body>
+ 
 </html>
