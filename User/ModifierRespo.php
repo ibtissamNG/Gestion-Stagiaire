5301b6beac70e878f481ec312b2fb33d1ed3351b
ouissalneqrouz <102753278+ouissalneqrouz@users.noreply.github.com>
2022-04-26 00:39:44 +0000

    Update ModifierRespo.php

--- User/ModifierRespo.php
+++ User/ModifierRespo.php
@@ -23,7 +23,7 @@
             <div class="container-fluid  dashboard-content">
                 
                 <div class="card">
-                    <h5 class="card-header">Modifier responsable</h5>
+                    <h2 class="card-header">Modifier responsable</h2>
                     <div class="card-body">
                         <?php 
                         include('../Connexion_database.php');
