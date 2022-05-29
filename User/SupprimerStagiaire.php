a35725e31f879057476142ca295a38ec6d036b9e
ibtissamNG <102632724+ibtissamNG@users.noreply.github.com>
2022-05-29 15:03:53 +0100

    Update SupprimerStagiaire.php

--- User/SupprimerStagiaire.php
+++ User/SupprimerStagiaire.php
@@ -5,12 +5,12 @@ include("../Connexion_database.php");
 $select = "delete from stagiaire where id_stagiaire='".$_GET['id']."'";
 if($query = mysqli_query($connexion,$select))
 {
-echo '<script>alert(\'Suppression avec succes :)\');</script>';
+echo '<script>alert(\'Suppression avec succès !)\');</script>';
 header('Refresh: 0; url=AfficherStagiaire.php');
 }
 else
 {
-echo '<script>alert(\'Une erreur est survenue Veuillez reessayer\');</script>';
+echo '<script>alert(\'Une erreur est survenue !! Veuillez ressayer\');</script>';
 header('Refresh: 0; url=AfficherStagiaire.php');
 }
 ?>
